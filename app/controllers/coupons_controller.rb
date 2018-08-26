8653797925183c3993ae94d9badbcde60d464ec9
Nancy Jo <enciie@gmail.com>
2018-08-26 08:52:18 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,20 @@
 class CouponsController < ApplicationController
+  def index
+  end
+
+  def show 
+    @coupons = Coupon.all
+  end
+
+  def new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+    redirect_to 
+  end
 
 end
